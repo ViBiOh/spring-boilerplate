49b11ad3fbf398a2e1b0d74e7cde4505187aa3d7
Vincent Boutour <ViBiOh@users.noreply.github.com>
2016-03-29 11:35:47 +0200

    Update Application.java

--- src/main/java/org/vibioh/config/Application.java
+++ src/main/java/org/vibioh/config/Application.java
@@ -1,18 +1,35 @@
 package org.vibioh.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @Configuration
 @EnableAutoConfiguration
 @EnableWebSocket
 @ComponentScan(value = "org.vibioh")
 @PropertySource("classpath:application.properties")
-public class Application {
-  public static void main(final String[] args) throws Exception {
-    SpringApplication.run(Application.class, args);
-  }
+public class Application implements WebSocketConfigurer {
+   @Autowired
+   private WebSocketHandler webSocketHandler;
+
+   @Override
+   public void registerWebSocketHandlers(final WebSocketHandlerRegistry webSocketHandlerRegistry) {
+	webSocketHandlerRegistry.addHandler(webSocketHandler, "/hello");
+
+	webSocketHandlerRegistry
+		.addHandler(webSocketHandler, "/hello")
+		.withSockJS();
+   }
+
+   public static void main(final String[] args) throws Exception {
+	SpringApplication.run(Application.class, args);
+   }
 }
