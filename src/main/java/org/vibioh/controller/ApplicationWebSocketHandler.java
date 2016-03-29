7992c32eca520c46d4c48ad93b0785a5173657dc
Vincent Boutour <ViBiOh@users.noreply.github.com>
2016-03-29 11:36:29 +0200

    Create ApplicationWebSocketHandler.java

---
+++ src/main/java/org/vibioh/controller/ApplicationWebSocketHandler.java
@@ -0,0 +1,39 @@
+package org.vibioh.controller;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.WebSocketMessage;
+import org.springframework.web.socket.WebSocketSession;
+
+@Component
+public class ApplicationWebSocketHandler implements WebSocketHandler {
+   private static final Logger logger = Logger.getLogger(ApplicationWebSocketHandler.class);
+
+   @Override
+   public void afterConnectionEstablished(final WebSocketSession webSocketSession) throws Exception {
+	logger.info("afterConnectionEstablished");
+   }
+
+   @Override
+   public void handleMessage(final WebSocketSession webSocketSession, final WebSocketMessage<?> webSocketMessage) throws Exception {
+	logger.info("handleMessage : " + webSocketMessage.getPayload());
+	webSocketSession.sendMessage(webSocketMessage);
+   }
+
+   @Override
+   public void handleTransportError(final WebSocketSession webSocketSession, final Throwable throwable) throws Exception {
+	logger.error("handleTransportError", throwable);
+   }
+
+   @Override
+   public void afterConnectionClosed(final WebSocketSession webSocketSession, final CloseStatus closeStatus) throws Exception {
+	logger.info("afterConnectionClosed Status : " + closeStatus);
+   }
+
+   @Override
+   public boolean supportsPartialMessages() {
+	return false;
+   }
+}
