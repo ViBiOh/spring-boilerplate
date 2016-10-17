28a6784aff6a8d856b7ca6c6d1e64cd1c2ca590c
Vincent Boutour <vincent.boutour@gmail.com>
2016-10-17 23:44:45 +0200

    Fixing import

--- src/main/java/org/vibioh/config/Application.java
+++ src/main/java/org/vibioh/config/Application.java
@@ -1,12 +1,12 @@
 package org.vibioh.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Bean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
